3ac51cb80c5e4365825533b0ddbc5fa960ea3c69
Meghna-bits <2021mt93670@wilp.bits-pilani.ac.in>
2023-04-22 20:50:33 +0530

    added forecast_preference get & put

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express')
+const DBUtilService = require('./DBUtilService')
+const cityToLocationKeyService = require('./LocationKeyService')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.put('/forecast_preference', jsonParser, (req, res) =>{
+    //TODO: change to actual userId
+    let userId = 1,
+    cities = req.body.cities,
+    locationKeys = cityToLocationKeyService.getLocationKeyForCities(cities);
+    DBUtilService.upsertForecastPreference(userId,locationKeys, (err, result) => {
+      if(err)
+        res.send(500, {message: 'Err in updating preferences!'})
+      res.send(200, {message: 'Your preference has been saved!'})
+    })
+})
+
+app.get('/forecast_preference', (req, res) => {
+    let userId = req.query['user_id'];
+    DBUtilService.getForecastPreference(userId, (err, result) => {
+      if(err)
+        res.send(500, {message: 'Err in updating preferences!'})
+
+      res.send(200, result)
+    })
+
 })
 
 app.listen(port, () => {
