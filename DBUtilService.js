6b1fb2c63da048bb56de2d97d7d1c730f79c83f9
Meghna-bits <2021mt93670@wilp.bits-pilani.ac.in>
2023-04-26 00:29:14 +0530

    change port

--- DBUtilService.js
+++ DBUtilService.js
@@ -8,7 +8,8 @@ initNewConnection = () => {
         host: process.env.DATABASE_HOST,
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE
+        database: process.env.DATABASE,
+        port: process.env.DATABASE_PORT
     })
     connection.connect()
     return connection;
